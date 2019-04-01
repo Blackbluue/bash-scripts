970283c16c3285a12fb40607753b0486768392cb
usacys <usacys@local>
2019-04-01 18:13:13 -0400

    -Update newpy.sh with more base syntax for the python file

--- newpy.sh
+++ newpy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 python_version="#!/usr/bin/env python3"
+python_syntax="\n
+def main():\n
+    return\n
+\n
+\n
+if __name__ == \"__main__\":\n
+    main()"
 file_name="$1.py"
 touch $file_name
 echo $python_version >> $file_name
+echo $python_syntax >> $file_name
 chmod a+x $file_name
