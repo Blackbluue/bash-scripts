a6d977b840a395d92c0e36688d6cd07068a13d95
rmiller <miller_raymone@hotmail.com>
2023-01-23 11:03:15 -0500

    Substitute spaces for underlines in main C files

--- newcmain.sh
+++ newcmain.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
-file_name="$1.c"
+#!/bin/bash
+file_name="${1// /_}.c"
 
 touch $file_name
 
-echo "int main(int argc, char *argv[])" >> $file_name
+echo "int main(int argc, char *argv[])" > $file_name
 echo "{" >> $file_name
-echo "\t// Place code here" >> $file_name
+echo -e "\t// Place code here" >> $file_name
 echo "}" >> $file_name
