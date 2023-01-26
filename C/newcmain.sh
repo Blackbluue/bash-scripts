7fa7da866e3c3541e0f427ae8f4fd803d96aac90
rmiller <miller_raymone@hotmail.com>
2023-01-26 11:14:55 -0500

    Use Heredoc

--- C/newcmain.sh
+++ C/newcmain.sh
@@ -3,7 +3,9 @@ file_name="${1// /_}.c"
 
 touch $file_name
 
-echo "int main(int argc, char *argv[])" > $file_name
-echo "{" >> $file_name
-echo -e "\t// Place code here" >> $file_name
-echo "}" >> $file_name
+cat > $file_name << EOM
+int main(int argc, char *argv[])
+{
+	// Place code here
+}
+EOM
